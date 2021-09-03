b8e4084b021020e7bb8d4d52bd214bf91f73c608
Matheus Paes de Andrade <bold.andrade@webmotors.com.br>
2021-09-03 12:16:44 -0300

    salvando

---
+++ teste_webmotorsWeb - Copia/Models/ModelosVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teste_webMotorsWeb.Models
+{
+    public class ModelosVM
+    {
+        public string ID { get; set; }
+        public int MakeID { get; set; }
+        public string Name { get; set; }
+    }
+}
